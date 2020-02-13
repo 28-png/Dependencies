4a5be139de519d1e7968be898882906e2323c3a4
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-13 12:55:24 -0600

    updated

--- src/main/java/StringManipulation.java
+++ src/main/java/StringManipulation.java
@@ -1,21 +1,25 @@
-import org.apache.commons.*;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Scanner;
 
 public class StringManipulation {
+
+
     public static void manipulateString() {
     String enter;
     Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Something");
         enter = scanner.next();
-
-
-
+        System.out.println("you entered: " + enter + "\n");
+        System.out.println("Flipped case: " + StringUtils.swapCase(enter) + "\n");
+        System.out.println("Reversed: " + StringUtils.reverse(enter));
     }
 
 
-    public static void main(String[] args) {
 
+
+    public static void main(String[] args) {
+        manipulateString();
 
     }
 
