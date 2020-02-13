f1ce459ac972dd6c060eb895a6b459fad79b898e
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-13 11:52:11 -0600

    updated

--- src/main/java/StringManipulation.java
+++ src/main/java/StringManipulation.java
@@ -1,5 +1,17 @@
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.*;
+
+import java.util.Scanner;
+
 public class StringManipulation {
+    public static void manipulateString() {
+    String enter;
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Something");
+        enter = scanner.next();
+
+
+
+    }
 
 
     public static void main(String[] args) {
